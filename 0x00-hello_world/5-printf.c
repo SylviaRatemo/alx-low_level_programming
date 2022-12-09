9fb2688cc6e1c0b294471af98cdf8a0c1a271094
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-08 18:22:33 -0800

    use of printf and puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,16 @@
 /*libraries*/
 #include<stdio.h>
 
-/*main function*/
+/**
+ *
+ * main function
+ */
 int main(void)
 {
-/*printf command*/
+/**
+ *
+ * printf command
+ */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
