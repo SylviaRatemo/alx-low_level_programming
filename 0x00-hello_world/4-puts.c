e002727fdaab96de701ef504035cae676fd03cb1
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-08 18:48:28 -0800

    Betty issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
-/* main function with return zero */
+/**
+ * main function with return zero
+ *
+ * Description: 
+ *
+ * Return:Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
