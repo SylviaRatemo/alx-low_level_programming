1035309baa68a0ab263989fbb7690b4be3ce2668
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-08 18:58:59 -0800

    errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  * 
- * Description: 'the program's description'
+ * Description:'the program's description'
  *
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
