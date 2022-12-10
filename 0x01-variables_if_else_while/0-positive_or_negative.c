2672b035827f2dfff79d00ba9698ead96bffe968
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-09 18:24:24 -0800

    if else nest

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
 int main(void)
 {
         int n;
@@ -13,17 +17,17 @@ int main(void)
                     /* your code goes there */
                 if (n > 0)
                 {
-                    printf("%d is positive",(signed int)n);
+                    printf("%d is positive ",(signed int)n);
                 }
                 else
                 {
                     if (n < 0)
                     {
-                        printf("%d is negative",(signed int)n);
+                        printf("%d is negative ",(signed int)n);
                     }
                     else
                     {
-                        printf("%d is zero",(signed int)n);
+                        printf("%d is zero ",(signed int)n);
                     }
                 }
                 return (0);
