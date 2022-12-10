7fd1cf5d55af7382fe74fae08315985cd4e4c000
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-09 18:34:11 -0800

    if else nest

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d is positive ", (signed int)n);
+printf("%d is positive\n", n);
 }
 else
 {
 if (n < 0)
 {
-printf("%d is negative ", (signed int)n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero ", (signed int)n);
+printf("%d is zero\n", n);
 }
 }
 return (0);
