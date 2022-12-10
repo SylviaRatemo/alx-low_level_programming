d3f5be835536ed66a3bc57f3b6d5113bc1163fb9
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-09 18:15:44 -0800

    if else nest

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@ int main(void)
             srand(time(0));
                 n = rand() - RAND_MAX / 2;
                     /* your code goes there */
-                if (r > 0)
+                if (n > 0)
                 {
                     printf("%d is positive",n);
                 }
                 else
                 {
-                    if (r = 0)
+                    if (n = 0)
                     {
                         printf("%d is zero",n);
                     }
