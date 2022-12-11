2c248d0cf7bbe3e27d0fb15052671209bfe2f0cc
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-11 01:06:42 -0800

    Advanced Tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint
+ * Return - returns 1
+ */
+
+int main(int argc, char** argv)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 60);
+	return 1;
