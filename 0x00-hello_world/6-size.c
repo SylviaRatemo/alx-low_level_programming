d0994b61dd16c88bec34d11d5796db3d2ccb8b94
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-11 00:13:24 -0800

    correction for 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,22 @@
-:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#include <stdio.h>
+
+/**
+ * Main - start of program
+ * Return - Always 0
+ */
+
+int main(void){
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of char: %d byte(s)", sizeof(c));
+	printf("Size of an int: %d byte(s)", sizeof(i));
+	printf("Size of a long int: %d byte(s)", sizeof(li));
+	printf("Size of a long long int: %d byte(s)", sizeof(lli));
+	printf("Size of a float: %d byte(s)", sizeof(f));
+
+	return (0);
+}
