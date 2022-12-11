1bfcffc2863280f58a1913220b497b8c02d2821b
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-11 01:17:45 -0800

    Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - entrypoint
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
