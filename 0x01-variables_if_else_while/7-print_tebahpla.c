d5528ae9bfec3039f55f5ba8fed2b7017eb0fd42
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 01:50:27 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
