43b398e154f06ef58bcc65ee43fbd0ef547f0365
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 12:40:09 -0800

    ädvanced while loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,24 @@
 
 int main(void)
 {
-	int num1 = 0;
-	int num2 = 0;
+	int num1, num2;
 
-	while (num2 < 10)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		putchar((num1 % 10) + '0');
-		putchar((num2 % 10) + '0');
-		num2++;
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	
+	putchar('\n');
 
 	return (0);
 }
