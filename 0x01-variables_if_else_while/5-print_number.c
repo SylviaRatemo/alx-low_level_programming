91159b0317373933de06e446667d35d9ab5bb918
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 00:42:26 -0800

    while loop to print numbers

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while(num <= 10)
+	{
+		printf("%d",num);
+	}
+	printf("\n");
+
+	return (0);
+}
