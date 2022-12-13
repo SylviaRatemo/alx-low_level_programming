cf30e1d053c93e8b9445877b11e4b3abc027d6da
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 00:26:48 -0800

    using while with an if condition

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main()
 {
 	char letter;
 
-	for (letter='a';letter<='z';letter++)
+	for (letter='a'; letter<='z'; letter++)
 	{
-		if (!(letter='e'||letter='q'))
+		if (!(letter='e' || letter='q'))
 		{
 			putchar(letter);
 		}
