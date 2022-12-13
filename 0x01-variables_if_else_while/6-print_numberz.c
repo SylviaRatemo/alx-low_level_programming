18a8fec5cc76152e03d344131c821186d523917b
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 01:30:45 -0800

    putchar with int

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,22 @@
  * Return:Always 0
  */
 
+void print_numbers(int n)
+{
+	if (n != 0)
+	{
+		print_numbers(n/10);
+		putchar((n%10) + '0');
+	}
+}
+
 int main(void)
 {
 	int num = 0;
 
 	while (num < 10)
 	{
-		putchar((char)num);
+		print_numbers(num);
 		num++;
 	}
 	putchar('\n');
