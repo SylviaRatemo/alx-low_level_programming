ffd7179df96abbb988e2497c882ca03ca86a2775
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 00:23:04 -0800

    using while with an if condition

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/** 
+ * main - entry
+ * Return - always 0
+ */
+
+int main()
+{
+	char letter;
+
+	for (letter='a';'letter<='z';letter++)
+	{
+		if !(letter='e'||letter='q')
+		{
+			putchar(letter);
+		}
+	}	
+	return (0);
+}
