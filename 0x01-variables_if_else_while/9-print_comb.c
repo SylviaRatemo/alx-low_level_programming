b1789e10c6b49bd653d1ba6f32635e19aa95f2f6
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-13 11:08:14 -0800

    single-digit numbers with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-Entry
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+		putchar(', ');
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
