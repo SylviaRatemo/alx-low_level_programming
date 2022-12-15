adb23a2796431f8d6bf326d465d31505c4e9731e
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-15 10:48:07 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 but 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num!= 4)
+		{
+			_putchar((num % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
