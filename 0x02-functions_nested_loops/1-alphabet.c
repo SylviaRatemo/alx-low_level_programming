291e4a237520553de9335d3307372460de4c9cb5
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-14 18:04:57 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
