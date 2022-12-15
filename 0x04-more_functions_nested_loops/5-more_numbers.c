83871c1a38297f380d5e38f716337fb6ed095587
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-15 11:04:14 -0800

    10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,15 @@ void more_numbers(void)
 {
 	int count, num;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar((num % 10) + '0');        
+			if (a > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar(a % 10 + '0');
 		}
 		_putchar('\n');
 	}
