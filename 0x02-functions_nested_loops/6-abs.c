6d1600c29fa56ec1d5c975ccb6d513b7124b234f
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-15 05:25:31 -0800

    absolute values

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,24 +7,6 @@
 
 int _abs(int c)
 {
-	int ab;
-
-	if (c < 0)
-	{
-		ab = c * (-1);
-		_putchar(ab);
-	}
-	else if (c > 0)
-	{
-		ab = c;
-		_putchar(ab);
-	}
-	else
-	{
-		ab = 0;
-		_putchar(ab);
-	}
-
-	return (0);
+	return (c * ((c > 0) - (c < 0)));
 }
 
