f541c7de93c6837ae4bb27e425a45f2ef7ceb34b
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-14 18:31:17 -0800

    prints alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	int letter = 'a';
+	int count, letter;
 
-	while (count < 10)
+	for(count = 0; count < 10; count++)
 	{
-		while (letter <= 'z')
+		for(letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			letter++;
 		}
 		_putchar('\n');
-		count++;
 	}
 }
