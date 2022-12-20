a21ca5a27b96ad9776432905b75bc905dfbd207a
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-20 15:14:38 -0800

    Swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+
+/**
+ * swap_int - swap two integer values
+ * @a: First integer
+ * @b: Second intege
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c = *a;
+	*a = *b;
+	*b = *c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,7 @@
 #define MAIN
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
 
 #endif
