c280967c78ce301d6da470424f214b1fe0377c34
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-21 01:46:40 -0800

    Reverse 1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,25 +2,6 @@
 #include <stdio.h>
 
 /**
- * str_len - get length of string
- * @s: String to be measured
- * Return: length of string
- */
-
-int str_len(char *s)
-{
-	int length, i;
-
-	for (i = 0; *(s + i) != 0; i++)
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-
-/**
  * print_rev - prints a string in reverse
  * @s: String to be reversed
  * Return: Nothing
@@ -31,7 +12,11 @@ void print_rev(char *s)
 	int length, i;
 	char *begin, *end, ch;
 
-	length = str_len(s);
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		length++;
+	}
+
 	begin = s;
 	end = s;
 
