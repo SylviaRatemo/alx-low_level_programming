d8cbce190da41db2a6f37f502ecc292567b62705
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-20 17:10:48 -0800

    Reverse
    git commit -m Reverse:

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: String to be reversed
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	/*int i = 0, length = 0;
+
+	for (; *(s + i) != 0; i++)
+	{
+		length++;
+	}*/
+
+	printf("%s", strrev(s);
+}	
