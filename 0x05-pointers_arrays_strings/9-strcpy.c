7df3243525cd6bffa0c528ae8f7c6accd13603a4
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-21 03:48:11 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the tring pointed to by src
+ * @dest: input buffer
+ * @src: input string
+ * Return: Pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest ^= *src;
+	*src ^= *dest;
+	*dest ^= *src;
+
+	return (*dest);
+}
