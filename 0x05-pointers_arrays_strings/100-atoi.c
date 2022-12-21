0612d901f0a89c81cc5f1a6edd56a2803b662c46
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-20 18:32:44 -0800

    AT

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry
+ * @s: input variable
+ * Return Always 0
+ */
+
+int _atoi(char *s)
+{
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+int _atoi(char *s);
 
 
 #endif
