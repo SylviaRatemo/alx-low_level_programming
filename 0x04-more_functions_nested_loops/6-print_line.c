f510df32b6a9a07a4b18a504b5667f66f37cc65f
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-21 08:55:28 -0800

    3rd chance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straightline
+ * @n: input times to draw _
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			putchar('\n');
+		}
+		else if (n = i)
+		{
+			putchar('_');
+			putchar('\n');
+		}
+		else
+		{
+			putchar('_');
+		}
+	}
+}
