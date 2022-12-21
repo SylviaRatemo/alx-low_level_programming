69df540995eb5924ac9c935dc8666c8aedaa5395
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-21 12:26:48 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,29 @@ int main(void)
 
 	for (; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n < 100)
 		{
-			printf("FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
+			if ((n % 3 == 0) && (n % 5 == 0))
+			{
+				printf("FizzBuzz");
+			}
+			else if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			printf(" ");
 		}
 		else
 		{
-			printf("%d", n);
-		}
-		printf(" ");
+			printf("Buzz\n");
 	}
 
 	return (0);
