39797de20fe8202eca4758952f4b19c14bc550d1
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-20 16:30:32 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string and new line to stdout
+ * @str: The string to be printed
+ * Returns: Nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}	
