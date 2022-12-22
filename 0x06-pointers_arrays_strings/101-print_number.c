885eec3b8ef48227b39b67f422187f419c2a139f
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 15:20:14 -0800

    Print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,5 +9,18 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('-);
+		num = -num;
+	}
+	
+	if((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+
 	putchar((n % 10) + '0');
 }
