db1ce46ba72b53a2eae76946e6b2eac41ee88fec
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 15:10:23 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot 13
+ * @str: Input string
+ * Returns: Pointer to encoded string
+ */
+
+char *rot13(char *str)
+{
+	int index1 = 0, index2 = 0;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	while (str[index1])
+	{
+		for(; index2 < 52; index2++)
+		{
+			if str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+		}
+
+		index1++;
+	}
+
+	return (str);
+}
