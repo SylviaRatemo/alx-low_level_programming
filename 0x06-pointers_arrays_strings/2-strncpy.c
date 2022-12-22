410991162e44afdbebc60ce5c2d32ba624eafeb6
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 07:27:15 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,51 +10,21 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, length = 0;
-	int j = 0, len = 0;
-	int destindex = 0, srcindex = 0;
-	int final = 0, diff = 0;
+	char *start = dest;
 
-	for (; i < *(dest + i); i++)
+	if ((dest == NULL) && (src == NULL))
 	{
-		length++;
+		return NULL;
 	}
 
-	destindex = length;
-
-	for (; j < *(src + j); j++)
+	while (*src && n--)
 	{
-		len++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
 
-	diff = (n - len);
-
-	if (diff >= n)
-	{
-		while (src[srcindex] != '\0')
-		{
-			dest[destindex] = src[srcindex];
-			destindex++;
-			srcindex++;
-		}
-	
-		while (diff != 0)
-		{
-			dest[destindex] = NULL;
-			destindex++;
-			diff--;
-		}
-	else
-	{
-		while ((src[srcindex] != '\0') && (srcindex >= n))
-		{
-			dest[destindex] = src[srcindex];
-			destindex++;
-			srcindex++;
-		}
-	}
-
-	dest[destindex] = '\0';
+	*dest = '\0';
 
 	return (dest);
 }
