2e1beae60bd8ea43f313e107b4bd3f0e3803d307
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 10:28:48 -0800

    ToUpper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @str: input string
+ * Return: Nothing
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0, index = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[index] >= 'a' || str[index] <= 'z')
+		{
+			str[index] = str[index] - 32;
+		}
+	}
+}
+		
