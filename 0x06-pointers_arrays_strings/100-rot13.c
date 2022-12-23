842e6621f328af3e9c5272f7657b322d9428ab79
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 21:52:41 -0800

    2nd

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,18 +3,18 @@
 /**
  * rot13 - encodes a string using rot 13
  * @str: Input string
- * Returns: Pointer to encoded string
+ * Return: Pointer to encoded string
  */
 
 char *rot13(char *str)
 {
 	int count = 0, i = 0;
-	char alphabet[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ"};
-	char rot13key[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','O','N','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char rot13key[] = {'n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'};
 
 	while (*(str + count) != '\0')
 	{
-		for(; i < 52; i++)
+		for (; i < 52; i++)
 		{
 			if (*(str + count) == alphabet[i])
 			{
