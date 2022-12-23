a559c315e99f554f34f9e4cb2afb4b746b7f9563
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 16:40:23 -0800

    Help

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,22 +8,22 @@
 
 char *rot13(char *str)
 {
-	int index1 = 0, index2 = 0;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int count = 0, i = 0;
+	char alphabet[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ"};
+	char rot13key[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-	while (str[index1])
+	while (*(str + count) != '\0')
 	{
-		for(; index2 < 52; index2++)
+		for(; i < 52; i++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (*(str + count) == alphabet[i])
 			{
-				str[index1] = rot13key[index2];
+				*(str + count) = rot13key[i];
 				break;
 			}
 		}
 
-		index1++;
+		count++;
 	}
 
 	return (str);
