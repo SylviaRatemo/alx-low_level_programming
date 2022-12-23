1b6b2340a1119f22e0d0f8d7adb1f0dfa8ab1774
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 16:32:31 -0800

    Help

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *str)
 	int count = 0, i = 0;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48. 55, 49};
+	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(str + count) != '\0')
 	{
