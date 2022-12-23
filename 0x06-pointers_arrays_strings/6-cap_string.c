0663a831f95bbbd2da6041b15ba3b37c426e14d2
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 21:14:22 -0800

    2nd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 		}
 
 		if (str[index - 1] == ' ' ||
-			str[index - 1] == '\t' ||
+				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
 				str[index - 1] == ';' ||
