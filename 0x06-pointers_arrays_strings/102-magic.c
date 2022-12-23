85bd95f51a004d2910047f14e66f2b882241e17a
SylviaRatemo <ratemosylvia@gmail.com>
2022-12-22 21:21:40 -0800

    2nd

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
+
+
 int main(void)
 {
        int n;
        int a[5];
        int *p;
-       
+
+
        a[2] = 1024;
        p = &n;
 
-       *(p + 5) = 98;
+
+       *(p + 5) = 9;
+
        
        printf("a[2] = %d\n", a[2]);
        
+
        return (0);
 }
