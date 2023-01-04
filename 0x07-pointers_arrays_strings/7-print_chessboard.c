9dd6e543398d47e7ffd2da1eccf78ce2041e9715
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-04 04:27:33 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 {
 	int i = 0, j = 0;
 
-	for (; i < *(a + i)*(a + i)*(a + i)*(a + i)*(a + i)*(a + i)*(a + i)*(a + i); i++)
+	for (; i < *(a + i); i++)
 	{
 		for (; j < 8; j++)
 		{
