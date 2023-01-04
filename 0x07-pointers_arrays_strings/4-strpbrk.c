e85c23f4aff935cbadadb015158b0de4995826be
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-04 05:48:51 -0800

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,26 +9,20 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int j = 0;
-	int flag;
+	int i = 0, j = 0;
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
-		flag = 0;
-		while (*(accept + j) != '\0')
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*s == *(accept + j))
+			if (s[i] == accept[j])
 			{
-				flag = 1;
+				return (s + 1);
 			}
 			j++;
 		}
-		j = 0;
-		if (flag == 1)
-		{
-			return (s);
-		}
-		s++;
+		i++;
 	}
 	return (NULL);
 }
