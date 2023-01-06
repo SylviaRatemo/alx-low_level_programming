4e9d918ebd8315869c8bf5c318a71f0483a88565
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-06 08:30:41 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -14,6 +14,6 @@ void _print_rev_recursion(char *s)
 
 	for (int i = length - 1; i >= 0; i--)
 	{
-		printf("%s", _print_rev_recursion(s[length - 2]));
+		printf("%s", _print_rev_recursion(s));
 	}
 }
