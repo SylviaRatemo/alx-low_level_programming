620023da61eea03aa46729a18be4882646d84cf6
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-06 12:06:34 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,22 +8,16 @@
 
 int factorial(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 	{
-		return (-1);
+		return (0);
 	}
-	else {
-		else if (n == 0)
-		{
-			return (0);
-		}
-		else if (n == 1)
-		{
-			return (1);
-		}
-		else
-		{
-			return (n * factorial(n - 1));
-		}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
 	}
 }
