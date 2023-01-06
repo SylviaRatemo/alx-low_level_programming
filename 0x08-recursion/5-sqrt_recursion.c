1312e19e304cb13c341d93cc07e8745d03625473
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-06 13:17:22 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,12 +9,23 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
 	else
-	{
-		return (((n * n) + (1 / x)) / 2);
-	}
+		return (squareroot(n, (n + 1) / 2));
+}
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }
