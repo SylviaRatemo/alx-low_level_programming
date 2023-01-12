5ef2c14dbdaac3fac7bb2d3cf6c343b26de43bf0
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-11 18:53:17 -0800

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,27 +19,27 @@ char *argstostr(int ac, char **av)
 
 	}
 	for (i = 0; i < ac; i++)
-											{
-														for (j = 0; av[i][j] != '\0'; j++)
-																		counter++;
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			counter++;
+		counter++;
+	}
+	counter++; /* to account for \n that needs to be added later*/
+	
+	ar = malloc(sizeof(char) * counter);
+	if (ar == NULL)
+		return (NULL);
 
-																counter++;
-																	}
-										counter++; /* to account for \n that needs to be added later */
-											ar = malloc(sizeof(char) * counter);
-												if (ar == NULL)
-															return (NULL);
-
-													for (n = 0; n < ac; n++)
-															{
-																		for (m = 0; av[n][m] != '\0'; m++)
-																					{
-																									ar[k] = av[n][m];
-																												k++;
-																														}
-																				ar[k] = '\n';
-																						k++;
-																							}
-														ar[k] = '\0';
-															return (ar);
+	for (n = 0; n < ac; n++)
+	{
+		for (m = 0; av[n][m] != '\0'; m++)
+		{
+			ar[k] = av[n][m];
+			k++;
+		}
+		ar[k] = '\n';
+		k++;
+	}
+	ar[k] = '\0';
+	return (ar);
 }
