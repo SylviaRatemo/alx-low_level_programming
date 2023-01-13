3689044cf5e88f124839d052064af26a66629172
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-13 06:55:34 -0800

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (ptr == NULL)
-		ptr = malloc(new_size);
+		ptr = malloc(sizeof(unsigned int) * new_size);
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 	free(ptr);
-	ptr = malloc(new_size);
+	ptr = malloc(sizeof(unsigned int) * new_size);
 
 	return (ptr);
 }
