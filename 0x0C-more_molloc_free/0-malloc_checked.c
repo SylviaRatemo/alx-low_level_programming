a513df49865c31f1699fa3acb8ab3ea7d76f96a6
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-13 00:57:46 -0800

    exi 98

---
+++ 0x0C-more_molloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: input b
+ * Return: Always 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ar;
+
+	ar = malloc(b);
+	if (ar == NULL)
+	{
+		exit (98);
+	}
+}
---
+++ 0x0C-more_molloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unisgned int size);
+int *array_range(int min, int max);
+
+
+#endif
