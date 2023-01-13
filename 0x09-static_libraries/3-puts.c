088e7428dbd128bbb9f745b99e484b65ea8bf6a2
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-12 21:17:40 -0800

    static

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Escribir un text
+ * @str: This is my entry
+ */
+
+void _puts(char *str)
+{
+	int a;
+	
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
