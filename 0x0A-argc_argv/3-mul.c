09c6f7c4c2e1b430a2762f465598d57540eda752
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-13 12:00:36 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1, mult;
+
+	if (argc > 1)
+	{
+		for (; i < argc; i++)
+		{
+			printf("%d", (int)argv[i]);
+			mult *= argv[i];
+		}
+	}
+
+	return (0);
+}
+
