85e49295d370686e1b81c04820bf11912c86dcc9
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-13 12:15:48 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,16 @@ int main(int argc, char *argv[])
 {
 	int i = 1, mult, num1, num2;
 
-	if (argc > 1)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-
-		mult = num1 * num2;
-		printf("%d\n", mult);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mult = num1 * num2;
+	printf("%d\n", mult);
 
 	return (0);
 }
