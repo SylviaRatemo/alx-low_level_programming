260aef7d63431ebb2177a01fda914c0e6862ea17
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-14 16:18:09 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,65 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * _atoi - converts a string to an ingeter
- * @s: string to be converted
- * Return: the int converted from the string
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: ALways 0 (Success)
  */
-int atoi(char *s)
+int check_num(char *str)
 {
-	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
+	unsigned int count = 0;
 
-	while (s[len] != '\0')
-		len++;
-		
-	while (i < len && f == 0)
+	while (count < strlen(str)) /*count string*/
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
-			f = 0;
+			return (0);
 		}
-		i++;
+		count++;
 	}
-	if (f == 0)
-		return (0);
-	return (n);
+	return (1);
 }
-
 /**
- * main - add positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: nothing
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: ALways 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int i = 0, num1, num2;
+	int count = 1, int str_to_int, sum = 0;
 
-	if (argc == 0)
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("%d\n", 0);
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
-
-	if !(argv >= '0' && argv <= '9')
-	{
-		printf("Error\n");
-	}
-	
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	
-	printf("%d\n", num1 + num2);
-
+	printf("%d\n", sum); /*print sum*/
 	return (0);
 }
