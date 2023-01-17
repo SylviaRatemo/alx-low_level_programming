2ea027102837850fdd2c84ac7fcd8258f917d3a9
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-17 12:22:23 -0800

    fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,9 @@ struct dog
 };
 
 #define dog_t (struct dog)
+dog_t *ne_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d,char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
