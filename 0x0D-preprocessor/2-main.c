0ed03241b2265a8af10c58594698925c32536105
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-17 05:33:41 -0800

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+/**
+ * main - print name of file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
