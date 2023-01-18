541b007ea6e73d3041cdb5c04dfe53089a514cba
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-18 00:50:05 -0800

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,11 @@ struct dog
 	char *owner;
 };
 
-void print_dog(struct dog *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void free_dog(dog_t *d);
 typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);i
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
