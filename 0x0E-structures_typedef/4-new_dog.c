c8ab3a87ca38a11392be64e3bed40d5646987971
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-18 02:46:32 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 
 	dog->name = name;
 	dog->age = age;
