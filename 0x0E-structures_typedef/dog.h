bc3351e6ea35d146a95ea7e8d49dc93310be3e2b
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-18 00:55:04 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
