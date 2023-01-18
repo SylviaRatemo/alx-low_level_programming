0bd1f363e6f629c2eb160cf13c54d54b4391e348
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-18 01:15:00 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * initi_dog - initializes dog
+ * @d: structure dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
