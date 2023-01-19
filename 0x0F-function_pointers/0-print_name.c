19e81fe58481e7a9704bb0ce14874b4635500c18
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-19 01:30:45 -0800

    task o

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: input string
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_name(char *name, void (*)(char *));
+
+
+#endif
