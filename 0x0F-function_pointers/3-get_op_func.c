8333da6da158d177b52ceb2a7aaedf4d29873143
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-19 04:41:29 -0800

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,13 +3,25 @@
 /**
  * get_op_func - selects pointer to correct function
  * @s: input string
- * @int: function parameters
  * Return: pointer to correct function
  */
 
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
 	if (s != '+' || s != '-' || s != '*' || s != '/' || s != '%')
 		return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
+	return (ops[i].f);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+ * main - compute
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always o
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = arg[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' %% num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+{
+
