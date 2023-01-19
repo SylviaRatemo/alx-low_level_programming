c62335b97868f9323e447ed779fd424c41893bec
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-19 03:49:27 -0800

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,10 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	while (i < size)
 	{
-		cmp(array[i]);
+		if (cmp(array[i]))
+			return (i);
 		i++;
 	}
-	if (cmp(array[i]) == 0)
-		return (-1);
-	return (i);
+	return (-1);
 }
