8d5b683f001ba76446256290afaf86d530ee1896
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-19 02:50:19 -0800

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function in parameter
+ * @array: input array
+ * @size: array size
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
