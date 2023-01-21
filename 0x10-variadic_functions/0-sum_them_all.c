575ba0a48249c72f95764de89c36a231d5c5a41c
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-21 05:37:59 -0800

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i = 0, sum = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(ap, count);
 
-	for(;i < n; i++)
+	for (; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
