cf6f300488cd5f81d271c7aeca24ee53cc3047cc
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-21 06:22:40 -0800

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @seperator: characteer between numbers
+ * @n: argument count
+ * Return: Nothing
+ */
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i = 0;
+
+	if (seperator == NULL)
+		return;
+
+	va_start(ap, n);
+
+	for (; i < n; i++)
+		printf("%d%c ", va_arg(ap, int), *seperator);
+
+}
