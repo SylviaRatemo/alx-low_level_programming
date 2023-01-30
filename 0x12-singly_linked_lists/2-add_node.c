6a11c147aa2b983f6d9653f6fa64fb6fb9b21969
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-30 06:39:39 -0800

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,14 +10,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int i = 0;
 	list_t *ptr = malloc(sizeof(list_t));
 
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	ptr->str = str;
-	ptr->next = *head;
+	ptr->str = strdup(str);
+	while (str[i] != '\0')
+		i++;
+	ptr->len = i;
+
+	if (*head != NULL)
+		ptr->next = *head;
+	if (*head == NULL)
+		ptr->next = NULL;
 	*head = ptr;
 
-	return (head);
+	return (*head);
 }	
