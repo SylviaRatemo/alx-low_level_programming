f37cfa313c0f3183171b6bbca816c1eec3b90177
SYLVIA RATEMO <ratemosylvia@gmail.com>
2023-01-30 03:32:49 -0800

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * size_t print_list - print elements of list_t
+ * @h: input struct
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	list_t *ptr = malloc(sizeof(list_t));
+	
+	ptr = h;
+	if (ptr->str == NULL)
+		printf("[0] (nil)");
+
+	while (ptr->next != NULL)
+	{
+		count++;
+		printf("[%d] %s\n", count, ptr->str);
+		ptr = ptr->next;
+
+	}
+	return (count);
+}
