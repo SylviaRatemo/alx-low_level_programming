f32660884daef7ebda024c0a2d559f8a45ecb0f7
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-30 04:16:56 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,8 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[%d} %s\n", 0, "(nil)");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
