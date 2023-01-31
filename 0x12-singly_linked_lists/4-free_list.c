97382d25ab49443a690dadcbd52360135529bd86
SylviaRatemo <ratemosylvia@gmail.com>
2023-01-30 18:17:47 -0800

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+ * free_list - frees linked list
+ * @head: input list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		free(ptr->str);
+		free(ptr);
+		ptr = ptr->next;
+	}
+}
