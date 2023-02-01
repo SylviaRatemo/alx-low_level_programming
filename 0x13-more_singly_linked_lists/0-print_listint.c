e8bb5391b6764ef2f26f584f337c38e5801c2c7d
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-01 08:14:39 -0800

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements
+ * @h: singly linked list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	do {
+		printf("%d\n", h->n);
+		i++;
+	} while (h->next != NULL);
+	return (i);
+}
