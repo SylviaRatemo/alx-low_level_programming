d0976f3c7d7de53c9cd8b3f2c18f0d8426f8fabe
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-01 09:26:39 -0800

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
-	
-	while (h->next != NULL)
-	{
+
+	do {
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
-	}
+	} while (h->next != NULL);
 	return (i);
 }
