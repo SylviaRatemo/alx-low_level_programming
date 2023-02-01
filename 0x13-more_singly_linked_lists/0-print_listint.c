d961f8042cf0dcd420878b000e6009a9952d640e
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-01 09:22:10 -0800

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -11,14 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
-	listint_t *head = malloc(sizeof(listint_t));
-
-	head = h;
-
-	do {
-		printf("%d\n", head->n);
-		head = head->next;
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
 		i++;
-	} while (head->next != NULL);
+	}
 	return (i);
 }
