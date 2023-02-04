f512b94677c6f797351493a97d6ec954ef977cd1
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-03 17:06:17 -0800

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free single linked list
+ * @head: double pointer to list
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t **ptr;
+
+	while (head)
+	{
+		ptr = head;
+		free(head);
+		head = ptr;
+	}
+	head = NULL;
+}
