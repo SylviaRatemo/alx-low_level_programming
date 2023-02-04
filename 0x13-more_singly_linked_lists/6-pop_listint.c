9529add29ef2568dbc62b69035d81255b638d927
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-03 17:25:58 -0800

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delete first node
+ * @head: double pointer to list
+ * Return: head node data or NULL if empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t **ptr;
+
+	ptr = head;
+
+	if (*head == NULL)
+		return (0);
+
+	*head = (*ptr)->next;
+
+	return ((*head)->n);
+}
