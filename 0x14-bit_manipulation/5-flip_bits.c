42c673653ad291ec54ca2fb6a23b26b6a15d33c9
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-06 18:05:56 -0800

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - count bits
+ * @n: input first number
+ * @m: input 2nd number
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+	return (count);
+}
