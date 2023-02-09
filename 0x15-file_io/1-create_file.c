0d56632beede0248779a2efaf51d6e4709e72331
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-08 18:38:39 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	sz = write(fd, text_content, len);
-	if (fd < 0 | sz < 0)
+	if (fd < 0 || sz < 0)
 		return (-1);
 
 	close(fd);
