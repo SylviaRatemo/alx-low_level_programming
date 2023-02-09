604cc0606249dc2a53e39cae3090d51ce596c6d4
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-08 18:37:35 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int sz, fd, len = 0;
-	
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	len = strlen(text_content);
-	sz = write(fd, text_content, len);
-	
 
-	if (fd < 0 | filename == NULL | sz < 0)
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	sz = write(fd, text_content, len);
+	if (fd < 0 | sz < 0)
 		return (-1);
 
 	close(fd);
