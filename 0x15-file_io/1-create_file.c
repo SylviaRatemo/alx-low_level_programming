69a20f035da08fb186b0c281c1926aab6470c752
SylviaRatemo <ratemosylvia@gmail.com>
2023-02-08 18:41:07 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 
 /**
  * create_file - creates a file
@@ -24,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	sz = write(fd, text_content, len);
-	if (fd < 0 || sz < 0)
+	if (fd == -1 || sz == -1)
 		return (-1);
 
 	close(fd);
