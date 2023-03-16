af459b9beed2696f8587658c2bb3bd7313dd7894
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-16 02:53:15 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t size = 0;
-
+	size_t size = 1;
+	
 	while (h->next != NULL)
 	{
 		printf("%d", h->n);
-		h = h->next;
+		h = h->next->next;
 		size++;
 	}
 	return (size)
