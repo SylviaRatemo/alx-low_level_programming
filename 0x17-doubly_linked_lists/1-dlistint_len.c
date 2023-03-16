6be15a4d3ffcf2048f9fd0a17eeb54fbe1ff5e71
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-16 05:59:02 -0700

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - get number of elements in list
+ * @h: pointer to list
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
