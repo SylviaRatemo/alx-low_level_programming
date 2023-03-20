80d97d9233ebfd58b310672c9b303c36e297b841
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-20 02:31:18 -0700

    18

---
+++ 0x18-dynamic_libraries/trial/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - This function is equal to strlen
+ * @s: this is my entr
+ * Return: the las character
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0' ; a++)
+	{
+	}
+	return (a);
+}
