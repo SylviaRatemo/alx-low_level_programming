5982f0268b701b09f4fe7fde1f1e088309078c7f
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-07 12:14:00 -0700

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	table->size = size;
+	
+	for(i = 0; i < table->size; i++)
+		table->array = NULL;
+
+	return table;
+}
