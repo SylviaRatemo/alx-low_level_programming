cc0cd547099132bdb0f854174c1e35079f62545f
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-07 12:22:40 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,10 +4,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
 	unsigned long int i;
+
+	if(size == 0)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_node_t*)*size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 	table->size = size;
 	
 	for(i = 0; i < table->size; i++)
-		table->array = NULL;
+		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
