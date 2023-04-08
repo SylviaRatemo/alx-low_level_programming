20e0b6b2ddac9b09f19c4d1b979c736a94f895e9
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-07 21:44:20 -0700

    Betty

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns index of key
+ * @key: the key
+ * @size: size of array of hash table
+ * Return: index of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
